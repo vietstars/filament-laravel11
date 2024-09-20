eb671c274958032b6fbe42c201ac340c1f35de33
Vietstar <vietstar.nt@gmail.com>
2024-09-20 10:17:02 +0700

    first build

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -43,5 +45,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**  
+     * set role can access panel
+     * 
+     * @return bool
+    */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // return $this->is_admin == 1;
+        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();;
     }
 }
